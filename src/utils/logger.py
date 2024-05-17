90d495fe81b7e51d72d7229d919aebcb869823d8
cbcote <christopher.cote88@gmail.com>
2024-05-17 17:08:30 -0500

    refactor: Update logger.py for improved logging functionality

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -5,10 +5,29 @@ import yaml
 class Logger:
     @staticmethod
     def setup_logging(config_path='config/logging_config.yaml', default_level=logging.INFO):
+        """
+        Setup logging configuration.
+        
+        Args:
+            config_path: Local path to the logging configuration file
+            default_level: Default logging level
+        
+        Returns:
+            None
+        """
         with open(config_path, 'r') as file:
             config = yaml.safe_load(file)
         logging.config.dictConfig(config)
 
     @staticmethod
-    def get_logger(logger_name):
+    def get_logger(logger_name: str) -> logging.Logger:
+        """
+        Get a logger instance.
+        
+        Args:
+            logger_name: Name of the logger
+        
+        Returns:
+            Logger instance
+        """
         return logging.getLogger(logger_name)
